0493e4e51b05c557789274a3cf956f129e17313e
Patrick Kimber <code@pkimber.net>
2013-09-05 20:12:55 +0100

    Test invoice lines

--- invoice/tests/model_maker.py
+++ invoice/tests/model_maker.py
@@ -3,6 +3,7 @@ from base.tests.model_maker import clean_and_save
 from invoice.models import (
     Invoice,
     InvoiceLine,
+    TimeRecord,
 )
 
 
@@ -24,5 +25,19 @@ def make_invoice_line(invoice, line_number, quantity, units, price, vat_rate):
             units=units,
             price=price,
             vat_rate=vat_rate,
+        )
+    )
+
+
+def make_time_record(ticket, user, description, date_started, start_time, end_time, billable):
+    return clean_and_save(
+        TimeRecord.objects.create(
+            ticket=ticket,
+            user=user,
+            description=description,
+            date_started=date_started,
+            start_time=start_time,
+            end_time=end_time,
+            billable=billable
         )
     )
---
+++ invoice/tests/test_invoice_line.py
@@ -0,0 +1,56 @@
+"""
+Test invoice line
+"""
+from datetime import datetime
+from datetime import time
+
+from django.test import TestCase
+
+from crm.tests.model_maker import make_contact
+from crm.tests.model_maker import make_priority
+from crm.tests.model_maker import make_ticket
+from invoice.tests.model_maker import make_invoice
+from invoice.tests.model_maker import make_invoice_line
+from invoice.tests.model_maker import make_time_record
+from login.tests.model_maker import make_user
+
+
+class TestInvoiceLine(TestCase):
+
+    def setUp(self):
+        tom = make_user('tom')
+        icl = make_contact('icl', 'ICL')
+        invoice = make_invoice(
+            datetime.today(),
+            icl,
+        )
+        self.invoice_line = make_invoice_line(
+            invoice, 1, 1.3, 'hours', 300.00, 0.20
+        )
+        self.invoice_line_with_time = make_invoice_line(
+            invoice, 2, 1.3, 'hours', 120.00, 0.20
+        )
+        ticket = make_ticket(
+            icl,
+            tom,
+            'Sew',
+            'Sewing',
+            make_priority('Low', 1),
+        )
+        time_record = make_time_record(
+            ticket,
+            tom,
+            'Set-up test framework',
+            date_started=datetime(2012, 11, 13),
+            start_time=time(9, 0),
+            end_time=time(9, 30),
+            billable=True,
+        )
+        time_record.invoice_line = self.invoice_line_with_time
+        time_record.save()
+
+    def test_has_time_record_not(self):
+        self.assertFalse(self.invoice_line.has_time_record)
+
+    def test_has_time_record(self):
+        self.assertTrue(self.invoice_line_with_time.has_time_record)
