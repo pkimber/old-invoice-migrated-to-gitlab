f8eb44a56167e74c96d893827194ca40d8e36d12
Patrick Kimber <code@pkimber.net>
2013-09-26 21:42:21 +0100

    version 0.0.07

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/invoice/*.*',
         ],
     },
-    version='0.0.06',
+    version='0.0.07',
     description='invoice',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
