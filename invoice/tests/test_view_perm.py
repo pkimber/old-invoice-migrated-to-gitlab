4fe1d6987baebc510918b921deca0e9143071fe2
Patrick Kimber <code@pkimber.net>
2013-10-12 20:46:19 +0100

    Use invoice scenario for 'test_view_perm'

--- invoice/tests/test_view_perm.py
+++ invoice/tests/test_view_perm.py
@@ -1,89 +1,76 @@
 from datetime import datetime
-from datetime import time
-from decimal import Decimal
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-from crm.tests.model_maker import (
-    make_contact,
-    make_note,
-    make_priority,
-    make_ticket,
-)
 from invoice.service import (
+    InvoiceCreate,
     InvoicePrint,
 )
-from invoice.tests.model_maker import (
-    make_invoice,
-    make_invoice_line,
-    make_invoice_settings,
-    make_time_record,
+from crm.tests.scenario import (
+    contact_contractor,
+    get_contact_farm,
+    get_contact_smallholding,
+    get_ticket_fence_for_farm,
 )
-from login.tests.model_maker import make_user
+from invoice.tests.scenario import (
+    get_timerecord_fence_dig_holes,
+    time_fencing,
+    time_paperwork,
+    invoice_settings,
+)
+from login.tests.scenario import (
+    get_user_staff,
+    get_user_web,
+    user_contractor,
+    user_default,
+)
 
 
 class TestViewPerm(TestCase):
 
     def setUp(self):
-        self.sam = make_user('sam', is_staff=True)
-        self.tom = make_user('tom')
+        user_contractor()
+        user_default()
+        contact_contractor()
+        invoice_settings()
+        time_fencing()
+        time_paperwork()
+        self.web = get_user_web()
         self.client.login(
-            username=self.tom.username, password=self.tom.username
-        )
-        self.icl = make_contact('icl', 'ICL', hourly_rate=Decimal('20.00'))
-        self.sew = make_ticket(
-            self.icl, self.tom, 'Sew', make_priority('Low', 1)
-        )
-        self.note = make_note(
-            self.sew, self.tom, 'Cut out some material and make a pillow case'
-        )
-        self.pillow = make_time_record(
-            self.sew,
-            self.tom,
-            'Make a pillow case',
-            datetime(2012, 9, 1),
-            time(9, 0),
-            time(12, 30),
-            True,
-        )
-        make_invoice_settings(
-            vat_rate=Decimal('0.20'),
-            vat_number='',
-            name_and_address='Patrick Kimber, Hatherleigh, EX20 1AB',
-            phone_number='01234 234 456',
-            footer="Please pay by bank transfer<br />Thank you"
+            username=self.web.username, password=self.web.username
         )
 
     def test_contact_invoice_list(self):
+        smallholding = get_contact_smallholding()
         url = reverse(
             'invoice.contact.list',
-            kwargs={'slug': self.icl.slug}
+            kwargs={'slug': smallholding.slug}
         )
         self._assert_get_perm_denied(url)
 
     def test_contact_time_record_list(self):
+        farm = get_contact_farm()
         url = reverse(
             'invoice.time.contact.list',
-            kwargs={'slug': self.icl.slug}
+            kwargs={'slug': farm.slug}
         )
         self._assert_get_perm_denied(url)
 
     def test_invoice_create(self):
+        farm = get_contact_farm()
         url = reverse(
             'invoice.create.time',
-            kwargs={'slug': self.icl.slug}
+            kwargs={'slug': farm.slug}
         )
         self._assert_post_staff_only(url)
 
     def test_invoice_download(self):
-        invoice = make_invoice(
-            self.tom,
-            invoice_date=datetime.today(),
-            contact=self.icl,
+        invoice = InvoiceCreate().create(
+            get_user_staff(),
+            get_contact_farm(),
+            datetime(2013, 12, 31)
         )
-        make_invoice_line(invoice, 1, 1.3, 'hours', 300.00, 0.20)
-        make_invoice_line(invoice, 2, 2.4, 'hours', 200.23, 0.20)
         InvoicePrint().create_pdf(invoice, header_image=None)
         url = reverse('invoice.download', kwargs={'pk': invoice.pk})
         self._assert_get_perm_denied(url)
@@ -93,7 +80,11 @@ class TestViewPerm(TestCase):
         self._assert_get_staff_only(url)
 
     def test_timerecord_create(self):
-        url = reverse('invoice.time.create', kwargs={'pk': self.sew.pk})
+        fence = get_ticket_fence_for_farm()
+        url = reverse(
+            'invoice.time.create',
+            kwargs={'pk': fence.pk}
+        )
         self._assert_get_staff_only(url)
 
     def test_timerecord_list(self):
@@ -101,11 +92,19 @@ class TestViewPerm(TestCase):
         self._assert_get_staff_only(url)
 
     def test_ticket_timerecord_list(self):
-        url = reverse('invoice.time.ticket.list', kwargs={'pk': self.sew.pk})
+        fence = get_ticket_fence_for_farm()
+        url = reverse(
+            'invoice.time.ticket.list',
+            kwargs={'pk': fence.pk}
+        )
         self._assert_get_perm_denied(url)
 
     def test_timerecord_update(self):
-        url = reverse('invoice.time.update', kwargs={'pk': self.pillow.pk})
+        dig = get_timerecord_fence_dig_holes()
+        url = reverse(
+            'invoice.time.update',
+            kwargs={'pk': dig.pk}
+        )
         self._assert_get_staff_only(url)
 
     def _assert_get_perm_denied(self, url):
@@ -115,7 +114,7 @@ class TestViewPerm(TestCase):
             403,
             "status {}: user '{}' should not have access "
             "to this url: '{}'".format(
-                response.status_code, self.tom.username, url
+                response.status_code, self.web.username, url
             )
         )
 
@@ -126,7 +125,7 @@ class TestViewPerm(TestCase):
             302,
             "status {}: user '{}' should not have access "
             "to this url: '{}'".format(
-                response.status_code, self.tom.username, url
+                response.status_code, self.web.username, url
             )
         )
 
@@ -137,6 +136,6 @@ class TestViewPerm(TestCase):
             302,
             "status {}: user '{}' should not have access "
             "to this url: '{}'".format(
-                response.status_code, self.tom.username, url
+                response.status_code, self.web.username, url
             )
         )
