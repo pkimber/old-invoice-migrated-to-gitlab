4f63e8fe69c591cb9c7c476242757cf9d2a38805
Patrick Kimber <code@pkimber.net>
2014-07-16 13:36:54 +0100

    factory - invoice line

--- invoice/tests/factories.py
+++ invoice/tests/factories.py
@@ -8,7 +8,10 @@ from datetime import date
 from crm.tests.factories import ContactFactory
 from login.tests.factories import UserFactory
 
-from invoice.models import Invoice
+from invoice.models import (
+    Invoice,
+    InvoiceLine,
+)
 
 
 class InvoiceFactory(factory.django.DjangoModelFactory):
@@ -19,3 +22,21 @@ class InvoiceFactory(factory.django.DjangoModelFactory):
     contact = factory.SubFactory(ContactFactory)
     invoice_date = date.today()
     user = factory.SubFactory(UserFactory)
+
+
+class InvoiceLineFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = InvoiceLine
+
+    price = 0
+    quantity = 1
+    vat_rate = 20
+
+    @factory.sequence
+    def line_number(n):
+        return n
+
+    @factory.lazy_attribute
+    def user(self):
+        return self.invoice.user
--- invoice/tests/test_credit_note.py
+++ invoice/tests/test_credit_note.py
@@ -1,9 +1,14 @@
 # -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
 
+from decimal import Decimal
+
 from django.test import TestCase
 
-from .factories import InvoiceFactory
+from .factories import (
+    InvoiceFactory,
+    InvoiceLineFactory,
+)
 
 
 class TestCreditNote(TestCase):
@@ -11,3 +16,6 @@ class TestCreditNote(TestCase):
     def test_factory(self):
         """ Create a simple credit note."""
         invoice = InvoiceFactory()
+        InvoiceLineFactory(invoice=invoice, price=Decimal('10.01'))
+        InvoiceLineFactory(invoice=invoice, price=Decimal('0.99'))
+        self.assertEqual(Decimal('11.00'), invoice.net)
