769a30ed1e770ef5e66870e234ec0146f2f27bc4
Patrick Kimber <code@pkimber.net>
2014-08-03 15:15:48 +0100

    version 0.1.08

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
             'templates/search/indexes/invoice/*.*',
         ],
     },
-    version='0.1.07',
+    version='0.1.08',
     description='invoice',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
