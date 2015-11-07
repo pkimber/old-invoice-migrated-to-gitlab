17a9cd8b3dbd948e486e9c3e422d2091f15727cc
Patrick Kimber <code@pkimber.net>
2015-11-07 23:49:37 +0000

    pytest and issues running a test near midnight #980

--- invoice/tests/test_invoice_correction.py
+++ invoice/tests/test_invoice_correction.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
+import pytest
+
 from datetime import date
 from dateutil.relativedelta import relativedelta
-
-from django.test import TestCase
 
 from finance.tests.factories import VatSettingsFactory
 from invoice.models import InvoiceError
@@ -16,113 +16,131 @@ from invoice.tests.factories import (
     TimeRecordFactory,
 )
 
-class TestInvoiceCorrection(TestCase):
+@pytest.mark.django_db
+def test_is_not_draft():
+    InvoiceSettingsFactory()
+    VatSettingsFactory()
+    tr = TimeRecordFactory()
+    invoice = InvoiceCreate().create(
+        tr.user, tr.ticket.contact, date.today()
+    )
+    assert invoice.is_draft is True
+    InvoicePrint().create_pdf(invoice, None)
+    assert invoice.is_draft is False
 
-    def test_is_not_draft(self):
-        InvoiceSettingsFactory()
-        VatSettingsFactory()
-        tr = TimeRecordFactory()
-        invoice = InvoiceCreate().create(
-            tr.user, tr.ticket.contact, date.today()
-        )
-        self.assertTrue(invoice.is_draft)
-        InvoicePrint().create_pdf(invoice, None)
-        self.assertFalse(invoice.is_draft)
 
-    def test_set_is_draft(self):
-        InvoiceSettingsFactory()
-        VatSettingsFactory()
-        tr = TimeRecordFactory()
-        invoice = InvoiceCreate().create(
-            tr.user, tr.ticket.contact, date.today()
-        )
-        self.assertTrue(invoice.is_draft)
-        InvoicePrint().create_pdf(invoice, None)
-        self.assertFalse(invoice.is_draft)
+@pytest.mark.django_db
+def test_set_is_draft():
+    InvoiceSettingsFactory()
+    VatSettingsFactory()
+    tr = TimeRecordFactory()
+    invoice = InvoiceCreate().create(
+        tr.user, tr.ticket.contact, date.today()
+    )
+    assert invoice.is_draft is True
+    InvoicePrint().create_pdf(invoice, None)
+    assert invoice.is_draft is False
+    invoice.set_to_draft()
+    assert invoice.is_draft is True
+
+
+@pytest.mark.django_db
+def test_set_is_draft_too_late():
+    """invoice can only be set back to draft on the day it is created."""
+    InvoiceSettingsFactory()
+    VatSettingsFactory()
+    tr = TimeRecordFactory()
+    TimeRecordFactory(ticket = tr.ticket)
+    invoice = InvoiceCreate().create(
+        tr.user, tr.ticket.contact, date.today()
+    )
+    invoice.invoice_date=date.today() + relativedelta(days=-1)
+    invoice.save()
+    assert invoice.is_draft is True
+    InvoicePrint().create_pdf(invoice, None)
+    assert invoice.is_draft is False
+    with pytest.raises(InvoiceError) as e:
         invoice.set_to_draft()
-        self.assertTrue(invoice.is_draft)
+    assert 'only set an invoice back to draft on the day' in str(e.value)
 
-    def test_set_is_draft_too_late(self):
-        """invoice can only be set back to draft on the day it is created."""
-        InvoiceSettingsFactory()
-        VatSettingsFactory()
-        tr = TimeRecordFactory()
-        TimeRecordFactory(ticket = tr.ticket)
-        invoice = InvoiceCreate().create(
-            tr.user, tr.ticket.contact, date.today()
-        )
-        invoice.invoice_date=date.today() + relativedelta(days=-1)
-        invoice.save()
-        self.assertTrue(invoice.is_draft)
-        InvoicePrint().create_pdf(invoice, None)
-        self.assertFalse(invoice.is_draft)
-        with self.assertRaises(InvoiceError):
-            invoice.set_to_draft()
 
-    def test_remove_time_lines(self):
-        """Remove all lines (because they are all linked to time records)."""
-        InvoiceSettingsFactory()
-        VatSettingsFactory()
-        tr = TimeRecordFactory()
-        TimeRecordFactory(ticket=tr.ticket)
-        invoice = InvoiceCreate().create(
-            tr.user, tr.ticket.contact, date.today()
-        )
-        self.assertTrue(invoice.is_draft)
-        InvoicePrint().create_pdf(invoice, None)
-        self.assertTrue(invoice.has_lines)
-        invoice.set_to_draft()
-        invoice.remove_time_lines()
-        self.assertFalse(invoice.has_lines)
+@pytest.mark.django_db
+def test_remove_time_lines():
+    """Remove all lines (because they are all linked to time records)."""
+    InvoiceSettingsFactory()
+    VatSettingsFactory()
+    tr = TimeRecordFactory()
+    TimeRecordFactory(ticket=tr.ticket)
+    invoice = InvoiceCreate().create(
+        tr.user, tr.ticket.contact, date.today()
+    )
+    assert invoice.is_draft is True
+    InvoicePrint().create_pdf(invoice, None)
+    assert invoice.has_lines is True
+    invoice.set_to_draft()
+    invoice.remove_time_lines()
+    assert invoice.has_lines is False
 
-    def test_remove_time_lines_not_extra(self):
-        """Remove all but one line.
 
-        The extra line is not removed because it isn't linked to a time record.
+@pytest.mark.django_db
+def test_remove_time_lines_not_extra():
+    """Remove all but one line.
 
-        """
-        InvoiceSettingsFactory()
-        VatSettingsFactory()
-        tr = TimeRecordFactory()
-        TimeRecordFactory(ticket=tr.ticket)
-        invoice = InvoiceCreate().create(
-            tr.user, tr.ticket.contact, date.today()
-        )
-        extra_line = InvoiceLineFactory(invoice=invoice)
-        self.assertTrue(invoice.is_draft)
-        InvoicePrint().create_pdf(invoice, None)
-        self.assertTrue(invoice.has_lines)
-        invoice.set_to_draft()
-        invoice.remove_time_lines()
-        self.assertListEqual(
-            [extra_line.pk,],
-            [i.pk for i in invoice.invoiceline_set.all()]
-        )
+    The extra line is not removed because it isn't linked to a time record.
 
-    def test_refresh(self):
-        """Create a draft invoice, and then add more time records to it."""
-        InvoiceSettingsFactory()
-        VatSettingsFactory()
-        tr = TimeRecordFactory()
-        invoice = InvoiceCreate().create(
-            tr.user, tr.ticket.contact, date.today()
-        )
-        self.assertEqual(1, invoice.invoiceline_set.count())
-        # create a couple more time records which can be added
-        TimeRecordFactory(ticket=tr.ticket)
-        TimeRecordFactory(ticket=tr.ticket)
+    .. important:: This test will fail if you try to run it around midnight.
+                   The end time will be something like ``time(0, 45)`` and
+                   the start time will be something like ``time(23, 45)``.
+                   Both are for the same date, and the end date will look as if
+                   it is before the start date!
+
+    """
+    InvoiceSettingsFactory()
+    VatSettingsFactory()
+    tr = TimeRecordFactory()
+    TimeRecordFactory(ticket=tr.ticket)
+    invoice = InvoiceCreate().create(
+        tr.user,
+        tr.ticket.contact,
+        date.today()
+    )
+    extra_line = InvoiceLineFactory(invoice=invoice)
+    assert invoice.is_draft is True
+    InvoicePrint().create_pdf(invoice, None)
+    assert invoice.has_lines is True
+    invoice.set_to_draft()
+    invoice.remove_time_lines()
+    assert [extra_line.pk] == [i.pk for i in invoice.invoiceline_set.all()]
+
+
+@pytest.mark.django_db
+def test_refresh():
+    """Create a draft invoice, and then add more time records to it."""
+    InvoiceSettingsFactory()
+    VatSettingsFactory()
+    tr = TimeRecordFactory()
+    invoice = InvoiceCreate().create(
+        tr.user, tr.ticket.contact, date.today()
+    )
+    assert 1 == invoice.invoiceline_set.count()
+    # create a couple more time records which can be added
+    TimeRecordFactory(ticket=tr.ticket)
+    TimeRecordFactory(ticket=tr.ticket)
+    InvoiceCreate().refresh(tr.user, invoice, date.today())
+    assert 3 == invoice.invoiceline_set.count()
+
+
+@pytest.mark.django_db
+def test_refresh_draft_only():
+    """Only draft invoices can be refreshed."""
+    InvoiceSettingsFactory()
+    VatSettingsFactory()
+    tr = TimeRecordFactory()
+    invoice = InvoiceCreate().create(
+        tr.user, tr.ticket.contact, date.today()
+    )
+    assert 1 == invoice.invoiceline_set.count()
+    InvoicePrint().create_pdf(invoice, None)
+    with pytest.raises(InvoiceError) as e:
         InvoiceCreate().refresh(tr.user, invoice, date.today())
-        self.assertEqual(3, invoice.invoiceline_set.count())
-
-    def test_refresh_draft_only(self):
-        """Only draft invoices can be refreshed."""
-        InvoiceSettingsFactory()
-        VatSettingsFactory()
-        tr = TimeRecordFactory()
-        invoice = InvoiceCreate().create(
-            tr.user, tr.ticket.contact, date.today()
-        )
-        self.assertEqual(1, invoice.invoiceline_set.count())
-        InvoicePrint().create_pdf(invoice, None)
-        with self.assertRaises(InvoiceError):
-            InvoiceCreate().refresh(tr.user, invoice, date.today())
+    assert 'Time records can only be added to a draft invoice' in str(e.value)
