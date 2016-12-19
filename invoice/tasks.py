243bb615c579c0598a2f2be5791d3d5092dcd556
Patrick Kimber <code@pkimber.net>
2016-12-19 20:12:43 +0000

    Remove HTML table (our mail cannot send HTML)

--- invoice/tasks.py
+++ invoice/tasks.py
@@ -22,29 +22,19 @@ def mail_time_summary():
     for user in users:
         logger.info('mail_time_summary: {}'.format(user.username))
         report = time_summary(user, days=1)
-        message = '<table border="0">'
+        message = ''
         for d, summary in report.items():
-            message = message + '<tr colspan="3">'
-            message = message + '<td>{}</td>'.format(d.strftime('%d/%m/%Y %A'))
-            message = message + '</tr>'
+            message = message + '\n\n{}, total time {}'.format(
+                d.strftime('%d/%m/%Y %A'),
+                summary['format_total'],
+            )
             for ticket in summary['tickets']:
-                message = message + '<tr>'
-                message = message + '<td>{}</td>'.format(ticket['pk'])
-                message = message + '<td>{}, {}</td>'.format(
+                message = message + '\n{}: {}, {} ({})'.format(
+                    ticket['pk'],
                     ticket['contact'],
                     ticket['description'],
-                )
-                message = message + '<td>{}</td>'.format(
                     ticket['format_minutes'],
                 )
-                message = message + '</tr>'
-            message = message + '<tr>'
-            message = message + '<td></td><td></td>'
-            message = message + '<td><b>{}</b></td>'.format(
-                summary['format_total']
-            )
-            message = message + '</tr>'
-        message = message + '</table>'
         queue_mail_message(
             user,
             [user.email],
