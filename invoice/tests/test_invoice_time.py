6aa60a92a0ef2da03587d51fc10c9a9db6e65724
Patrick Kimber <code@pkimber.net>
2017-04-27 13:15:46 +0100

    Convert to pytest

--- invoice/tests/test_invoice_time.py
+++ invoice/tests/test_invoice_time.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
+import pytest
+
 from datetime import date
 from decimal import Decimal
-
-from django.test import TestCase
 
 from contact.tests.factories import ContactFactory
 from crm.tests.factories import TicketFactory
@@ -19,38 +19,38 @@ from invoice.tests.factories import (
 )
 
 
-class TestInvoice(TestCase):
+@pytest.mark.django_db
+def test_invoice_with_time_records():
+    """Invoice time records."""
+    InvoiceSettingsFactory()
+    VatSettingsFactory()
+    contact = ContactFactory()
+    InvoiceContactFactory(contact=contact)
+    ticket = TicketFactory(contact=contact)
+    tr1 = TimeRecordFactory(ticket=ticket)
+    TimeRecordFactory(ticket=ticket)
+    invoice = InvoiceCreate().create(
+        tr1.user, contact, date.today()
+    )
+    assert invoice.is_draft
+    InvoicePrint().create_pdf(invoice, None)
+    assert not invoice.is_draft
+    assert Decimal('40.00') == invoice.net
 
-    def setUp(self):
-        InvoiceSettingsFactory()
 
-    def test_invoice_with_time_records(self):
-        """Invoice time records."""
-        VatSettingsFactory()
-        contact = ContactFactory()
-        InvoiceContactFactory(contact=contact)
-        ticket = TicketFactory(contact=contact)
-        tr1 = TimeRecordFactory(ticket=ticket)
-        TimeRecordFactory(ticket=ticket)
-        invoice = InvoiceCreate().create(
-            tr1.user, contact, date.today()
-        )
-        assert invoice.is_draft
-        InvoicePrint().create_pdf(invoice, None)
-        assert not invoice.is_draft
-        assert Decimal('40.00') == invoice.net
-
-    def test_invoice_with_time_records_no_end_time(self):
-        """One of the time records has no end time, so cannot be invoiced."""
-        VatSettingsFactory()
-        contact = ContactFactory()
-        InvoiceContactFactory(contact=contact)
-        ticket = TicketFactory(contact=contact)
-        tr1 = TimeRecordFactory(ticket=ticket)
-        TimeRecordFactory(ticket=ticket, end_time=None)
-        TimeRecordFactory(ticket=ticket)
-        with self.assertRaises(InvoiceError) as ex:
-            InvoiceCreate().create(tr1.user, contact, date.today())
-        message = str(ex.exception)
-        assert 'does not have a' in message
-        assert 'end time' in message
+@pytest.mark.django_db
+def test_invoice_with_time_records_no_end_time():
+    """One of the time records has no end time, so cannot be invoiced."""
+    InvoiceSettingsFactory()
+    VatSettingsFactory()
+    contact = ContactFactory()
+    InvoiceContactFactory(contact=contact)
+    ticket = TicketFactory(contact=contact)
+    tr1 = TimeRecordFactory(ticket=ticket)
+    TimeRecordFactory(ticket=ticket, end_time=None)
+    TimeRecordFactory(ticket=ticket)
+    with pytest.raises(InvoiceError) as ex:
+        InvoiceCreate().create(tr1.user, contact, date.today())
+    message = str(ex.value)
+    assert 'does not have a' in message
+    assert 'end time' in message
